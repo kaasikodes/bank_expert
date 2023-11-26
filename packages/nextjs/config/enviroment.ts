d8161fbac6a95cb1ea33cea6238f64fa2659aeda
kaasikodes <thisbroisfresh@gmail.com>
2023-11-26 17:13:50 +0100

    chore: updates enviroment object

--- packages/nextjs/config/enviroment.ts
+++ packages/nextjs/config/enviroment.ts
@@ -1,6 +1,8 @@
 const ENV = {
   TWITTER_CLIENT_ID: process.env.TWITTER_CLIENT_ID ?? "",
   TWITTER_CLIENT_SECRET: process.env.TWITTER_CLIENT_SECRET ?? "",
+  GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID ?? "",
+  GITHUB_CLIENT_SECRET: process.env.GITHUB_CLIENT_SECRET ?? "",
   NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET ?? "",
   NEXTAUTH_URL: process.env.NEXTAUTH_URL ?? "",
   NODE_ENV: process.env.NODE_ENV ?? "",
