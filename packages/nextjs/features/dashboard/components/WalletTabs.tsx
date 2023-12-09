e15cef7ca49fdc86b389b08f758242373dc4a608
kaasikodes <thisbroisfresh@gmail.com>
2023-12-09 11:44:47 +0100

    feat: adds refresh btn for getting wallets

--- packages/nextjs/features/dashboard/components/WalletTabs.tsx
+++ packages/nextjs/features/dashboard/components/WalletTabs.tsx
@@ -4,49 +4,62 @@ import WalletTableContainer from "./WalletTableContainer";
 import { Avatar, Segmented, Tabs } from "antd";
 // import { useSession } from "next-auth/react";
 import { useNetwork } from "wagmi";
+import { ArrowPathIcon } from "@heroicons/react/24/outline";
 import { useGetWalletCategories } from "~~/hooks/wallet/category/useGetWalletCategories";
 
 const WalletTabs = () => {
   // const { data: session } = useSession();
 
   const [selectedCoin, setSelectedCoin] = useState<string>("CCIP-BnM");
-  const { result: deployedWallets, isFetching: isFetchingDeployedWallets } = useGetDeployedWallets();
+  const { result: deployedWallets, isFetching: isFetchingDeployedWallets, refetch } = useGetDeployedWallets();
   const { chain } = useNetwork();
   const { data: categories, isLoading } = useGetWalletCategories();
   // const { data: categories, isLoading } = useGetWalletCategories({ userId: session?.user.id });
   return (
-    <Tabs
-      tabBarExtraContent={
-        <Segmented
-          onChange={val => setSelectedCoin(val as unknown as string)}
-          options={["CCIP-BnM", "CCIP-LnM", "Link"]}
-        />
-      }
-      items={[
-        {
-          key: "All",
-          label: <TabLabel amount={deployedWallets?.length} title={"All"} />,
-          children: (
-            <WalletTableContainer
-              selectedCrossChainCoin={selectedCoin}
-              addresses={deployedWallets}
-              isLoading={isFetchingDeployedWallets}
-            />
-          ),
-        },
-        ...(categories?.data ?? []).map(category => ({
-          key: category.name,
-          label: <TabLabel amount={chain ? category?.wallets?.length : 0} title={category.name} />,
-          children: (
-            <WalletTableContainer
-              selectedCrossChainCoin={selectedCoin}
-              isLoading={isLoading}
-              addresses={chain ? category.wallets.map(item => item.address) : []}
-            />
-          ),
-        })),
-      ]}
-    />
+    <div className="flex  flex-col gap-4">
+      <div className="flex justify-end">
+        <button className="btn btn-ghost btn-xs" onClick={async () => await refetch()}>
+          {isFetchingDeployedWallets ? (
+            <span className="loading loading-spinner loading-xs"></span>
+          ) : (
+            <ArrowPathIcon className="h-3 w-3 cursor-pointer" aria-hidden="true" />
+          )}{" "}
+          <span>Refresh</span>
+        </button>
+      </div>
+      <Tabs
+        tabBarExtraContent={
+          <Segmented
+            onChange={val => setSelectedCoin(val as unknown as string)}
+            options={["CCIP-BnM", "CCIP-LnM", "Link"]}
+          />
+        }
+        items={[
+          {
+            key: "All",
+            label: <TabLabel amount={deployedWallets?.length} title={"All"} />,
+            children: (
+              <WalletTableContainer
+                selectedCrossChainCoin={selectedCoin}
+                addresses={deployedWallets}
+                isLoading={isFetchingDeployedWallets}
+              />
+            ),
+          },
+          ...(categories?.data ?? []).map(category => ({
+            key: category.name,
+            label: <TabLabel amount={chain ? category?.wallets?.length : 0} title={category.name} />,
+            children: (
+              <WalletTableContainer
+                selectedCrossChainCoin={selectedCoin}
+                isLoading={isLoading}
+                addresses={chain ? category.wallets.map(item => item.address) : []}
+              />
+            ),
+          })),
+        ]}
+      />
+    </div>
   );
 };
 
